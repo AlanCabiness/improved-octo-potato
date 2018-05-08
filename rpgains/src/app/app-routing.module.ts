fc9b4bb214e8fa5e8af556e235e2c43b3a875668
Zizzybalooba <juliogiron5@gmail.com>
2018-05-07 20:10:55 -0500

    Inventory stuff done Hover function works Using card deck instead of Bootstrap

--- rpgains/src/app/app-routing.module.ts
+++ rpgains/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {HomepageComponent} from "./homepage/homepage.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {InventoryComponent} from "./inventory/inventory.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomepageComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'inventory', component: InventoryComponent}
 ];
 
 @NgModule({
--- rpgains/src/app/app.module.ts
+++ rpgains/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {AppComponent} from './app.component';
 import {HomepageComponent} from './homepage/homepage.component';
 import {AppRoutingModule} from './app-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    DashboardComponent
+    DashboardComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
