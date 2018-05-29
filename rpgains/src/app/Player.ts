6f655a865f68eee45e9799de4835967881e40fdb
Alejandro Vicuna <vicunaalejandro@gmail.com>
2018-05-29 15:41:26 -0500

    Added loot roll pictures for all loot types Started Player class initialization

--- rpgains/src/app/Player.ts
+++ rpgains/src/app/Player.ts
@@ -5,7 +5,24 @@ import { Weapon } from './Weapon';
 export class Player {
   id: number;
   name: string;
+  xp: number;
   helmet: Helmet[];
   armor: Armor[];
   weapon: Weapon[];
+
+  constructor() {
+    this.id = 1;
+    this.name = 'Temp';
+    this.xp = 200;
+    this.helmet = [];
+    this.armor = [];
+    this.weapon = [];
+  }
+
+  clear () {
+    this.helmet = undefined;
+    this.armor = undefined;
+    this.weapon = undefined;
+}
+
 }
--- rpgains/src/app/dashboard/dashboard.component.ts
+++ rpgains/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../Player';
 
 
 @Component({
@@ -9,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
+
+  temp = new Player();
 
   ngOnInit() {
   }
